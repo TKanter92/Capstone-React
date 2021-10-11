8770d639f32922469f118743e6175a13aecc967f
TKanter92 <TylerKanter@yahoo.com>
2021-10-11 09:57:14 -0500

    created Register Component

---
+++ capstone-react/src/components/Register/Register.jsx
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+            email: "",
+            firstName: "",
+            lastName: "",
+            user: props.user,
+            register: props.register
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.getRegistrationInfo();
+        this.props.navToRegister();
+    }
+
+    getRegistrationInfo = async () => {
+        const registrationInfo = {
+            username: this.state.username,
+            password: this.state.password,
+            email: this.state.email,
+            firstName: this.state.firstName,
+            lastName: this.state.lastName
+        };
+
+        try{
+            let response = await axios.post("http://127.0.0.1:8000/api/auth/register/", registrationInfo);
+            console.log(response);
+        }
+        catch{
+            console.log("Unsuccessful Registration");
+        }
+    }
+
+    render() {
+        return(
+            <div className="d-flex container justify-content-center align-items-center">
+                <form className="form-group" onSubmit={(event) => this.handleSubmit(event)}>
+                    <div className="row mb-3">
+                        <label>Username:</label>
+                        <div className="col-sm-10">
+                            <input type="text" name="username" placeholder="Enter Username..." onChange={this.handleChange} value={this.state.username} /><br />
+                        </div>
+                    </div>
+                    <div className="row mb-3">
+                        <label>Password:</label>
+                        <div className="col-sm-10">
+                            <input type="password" name="password" placeholder="Enter Password..." onChange={this.handleChange} value={this.state.password} /><br />
+                        </div>
+                    </div>
+                    <div className="row mb-3">
+                        <label>Email:</label>
+                        <div className="col-sm-10">
+                            <input type="email" name="email" placeholder="Enter Valid Email..." onChange={this.handleChange} value={this.state.email} /><br />
+                        </div>
+                    </div>
+                    <div className="row mb-3">
+                        <label>First Name:</label>
+                        <div className="col-sm-10">
+                            <input type="text" name="firstName" placeholder="Enter First Name..." onChange={this.handleChange} value={this.state.firstName} /><br />
+                        </div>
+                    </div>
+                    <div className="row mb-3">
+                        <label>Last Name:</label>
+                        <div className="col-sm-10">
+                            <input type="text" name="lastName" placeholder="Enter Last Name..." onChange={this.handleChange} value={this.state.lastName} /><br />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-10 col-auto">
+                            <button type="submit" className="btn btn-primary">Register</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Register;
